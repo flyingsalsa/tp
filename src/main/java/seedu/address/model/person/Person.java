3165a9dea19404d4af7f0ac94ca4a17eac4216de
flyingsalsa <107453007+flyingsalsa@users.noreply.github.com>
2024-10-03 07:36:33 +0800

    Finish New command tutorial

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -23,11 +23,21 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private Remark remark = new Remark("");
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Remark remark) {
+        requireAllNonNull(name, phone, email, address, tags);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.remark = remark;
+        this.tags.addAll(tags);
+    }
     public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
@@ -36,7 +46,6 @@ public class Person {
         this.address = address;
         this.tags.addAll(tags);
     }
-
     public Name getName() {
         return name;
     }
@@ -51,6 +60,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Remark getRemark() {
+        return remark;
     }
 
     /**
@@ -110,6 +123,7 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
+                .add("remark", remark)
                 .add("tags", tags)
                 .toString();
     }
